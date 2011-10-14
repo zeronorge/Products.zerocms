959957260ea8076bf3cd1d53354e2a5bdb5a1cc5
Tarjei Huse <tarjei@scanmine.com>
2011-10-14 15:35:10 +0200

    Remove files copied from collective.solr that were not needed

--- Products/zerocms/browser/resources/scripts.js
+++
@@ -1,9 +0,0 @@
-/* use `collective.js.showmore` to hide facet counts via jquery */
-(function($) {
-$(function() {
-    $('#portal-searchfacets dl').showMore({
-        expression: 'dt:gt(4), dd:gt(4)',
-        grace_count: 2
-    });
-});
-})(jQuery);
